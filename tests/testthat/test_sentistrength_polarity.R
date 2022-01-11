3f6898849daa738905d534fcb904fb68bc31006e
Claes Maëlick <himself@maelick.net>
2022-01-11 13:27:40 +0200

    Tests fix

--- tests/testthat/test_sentistrength_polarity.R
+++ tests/testthat/test_sentistrength_polarity.R
@@ -1,15 +1,15 @@
 context("sentistrength_polarity")
 
 test_that("Polarity with default limit", {
-  res <- SentiStrengthPolarity(c(1, 2, 1, 3, 5, 4, 4),
+  res <- Polarity(c(1, 2, 1, 3, 5, 4, 4),
                                c(-1, -1, -2, -3, -4, -5, -4))
   expect_equal(res, c(0, 1, -1, 0, 1, -1, NA))
 })
 
 test_that("Polarity with limit = 6", {
-  expect_equal(SentiStrengthPolarity(5, -5, 6), 0)
+  expect_equal(Polarity(5, -5, 6), 0)
 })
 
 test_that("Polarity with limit = 2", {
-  expect_equal(SentiStrengthPolarity(c(1, 2), c(-1, -2), 2), c(0, NA))
+  expect_equal(Polarity(c(1, 2), c(-1, -2), 2), c(0, NA))
 })
